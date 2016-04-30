2278457cadd633749bdc88af9cddd68e8d6ba92d
Vit Brunner <vit.brunner@gmail.com>
2016-04-30 11:43:23 +0200

    More future

--- test/setup/Fixtures.scala
+++ test/setup/Fixtures.scala
@@ -3,6 +3,7 @@ package test.setup
 import scala.concurrent._
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.language.postfixOps
 import scala.util._
 
 import models._
@@ -17,19 +18,23 @@ trait Fixtures extends Inject {
   )
 
   def before() = {
-    val deletes = Future.sequence(List(languageRepository.delete, languagePairRepository.delete))
+    val deletes: Future[List[Int]] = Future.sequence(List(languageRepository.delete, languagePairRepository.delete))
 
-    val insertedLanguages: Future[List[Language]] = Future.sequence(languages.map(languageRepository.insert(_)))
+    val insertedLanguages: Future[List[Language]] = deletes.flatMap {
+      case _ => Future.sequence(languages.map(languageRepository.insert(_)))
+    }
 
-    insertedLanguages andThen {
-      case Success(lang) => {
+    val insertedLanguagePairs: Future[Int] = insertedLanguages.flatMap {
+      case languages => {
         val languagePair = LanguagePair(
-          fromLanguage = lang(0),
-          toLanguage = lang(1),
+          fromLanguage = languages(0),
+          toLanguage = languages(1),
           maintainers = None)
 
         languagePairRepository.insert(languagePair)
       }
     }
+
+    Await.result(insertedLanguages, 1 seconds);
   }
 }
